7c92c535b3ade8c912157bbcfb3e3fb788a4a247
dcmartin <github@dcmartin.com>
2020-08-22 14:13:57 -0700

    lgtm

--- test.sh
+++ test.sh
@@ -1,20 +1,49 @@
 #!/bin/bash
 
-jq -c '.entity[]' labels.json \
-  | while read; do \
-    isa=$(echo "${REPLY}" | jq -c '.isa') \
-    && \
-    label=$(echo "${REPLY}" | jq -r '.label[].en') \
-    && \
-    echo -n "${label} isa:" \
-    &&
-    for c in $(echo "${isa}" | jq -r '.[]'); do \
-      echo -n " ${c}:" \
-      &&
-      class=$(jq '.isa[]|select(.id=="'${c}'")' labels.json) \
-      && \
-      if [ ! -z "${class:-}" ]; then echo -n " $(echo "${class}" | jq '.label[].en')"; else echo -n " NOT FOUND: ${c}"; fi; \
-    done \
-    && \
-    echo ''; \
+## labels
+LABELS=${0%%/*}/labels.json
+if [ ! -s "${LABELS}" ]; then
+  echo "No labels: ${LABELS}" &> /dev/stderr
+  exit 1
+fi
+
+## stuff
+DOCS=${0%%/*}/docs/stuff.txt
+if [ ! -s "${DOCS}" ]; then
+  echo "No stuff: ${DOCS}" &> /dev/stderr
+  exit 1
+fi
+
+## output
+out=${LABELS%%.json*}.$$.json
+
+## main
+echo '{"path":"'${out}'","coco":[' > ${out}
+j=0; jq -c '.entity[]' ${LABELS} | while read; do
+  isa=$(echo "${REPLY}" | jq -c '.isa')
+  label=$(echo "${REPLY}" | jq -r '.label[].en')
+
+  if [ ${j:-0} -gt 0 ]; then echo ',' >> ${out}; fi
+  echo '{"label":"'${label}'","isa":[' >> ${out}
+
+  i=0; for c in $(echo "${isa}" | jq -r '.[]'); do
+    if [ ${i:-0} -gt 0 ]; then echo -n ',' >> ${out}; fi
+    echo '{"id":"'${c}'","label":' >> ${out}
+    class=$(jq '.isa[]|select(.id=="'${c}'")' labels.json)
+    if [ ! -z "${class:-}" ]; then
+      n=$(echo "${class}" | jq '.label[].en')
+      ci=$(egrep "${label}"'$' ${DOCS} | awk -F': ' '{ print $1 }')
+      cs=$(egrep "${label}"'$' ${DOCS} | awk -F': ' '{ print $2 }')
+      echo ${n} >> ${out}
+    else
+      echo 'null' >> ${out}
+      echo "LABEL: ${label}; ISA: ${c}; NOT FOUND" &> /dev/stderr
+    fi
+    i=$((i+1))
+    echo '}' >> ${out}
   done
+  echo '],"stuff":{"id":"'${ci:-not found}'","label":"'${cs:-not found}'"}}' >> ${out}
+  j=$((j+1))
+done
+echo ']}' >> ${out}
+jq -c '.' ${out}
